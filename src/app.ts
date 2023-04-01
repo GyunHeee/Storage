31bfbc0afbcf93a561b00a8e4241871d81ed40c8
GyunHee <ef3232@naver.com>
2023-04-01 11:24:53 +0900

    비디오 구현

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { NoteComponent } from './components/page/item/Note.js';
 import { TodoComponent } from './components/page/item/Todo.js';
+import { VideoComponent } from './components/page/item/Video.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from './components/page/page.js';
 
@@ -9,11 +10,11 @@ class App {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
-    // const video = new VideoComponent(
-    //   'video Title',
-    //   'https://www.youtube.com/watch?v=3-BZxvKT6x4'
-    // );
-    // video.attachTo(appRoot, 'beforeend');
+    const video = new VideoComponent(
+      'video Title',
+      'https://www.youtube.com/watch?v=3-BZxvKT6x4'
+    );
+    video.attachTo(appRoot, 'beforeend');
 
     const image = new ImageComponent(
       'image Title',
--- src/components/page/item/Video.ts
+++ src/components/page/item/Video.ts
@@ -1,25 +1,35 @@
-// import { BaseComponent } from '../components.js';
+import { BaseComponent } from '../components.js';
 
-// export class VideoComponent extends BaseComponent<HTMLElement> {
-//   constructor(title: string, url: string) {
-//     super(`<section>
-//             <div class="video__holder">
-//                 <iframe class="video__thumnail" frameborder="0"></iframe>
-//             </div>
-//             <p class="video__title"></p>
-//         </section>`);
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section>
+            <div class="video__player">
+                <iframe class="video__iframe"></iframe>
+            </div>
+            <h3 class="video__title"></h3>
+        </section>`);
 
-//     // 사용자에게 전달받은 데이터를 innerHTML에 설정하는 것은 좋지 않다.
+    // 사용자에게 전달받은 데이터를 innerHTML에 설정하는 것은 좋지 않다.
 
-//     const videoElement = this.element.querySelector(
-//       '.video__thumnail'
-//     )! as HTMLImageElement;
-//     videoElement.src = url;
-//     videoElement.alt = title;
+    const iframe = this.element.querySelector(
+      '.video__iframe'
+    )! as HTMLIFrameElement;
+    console.log(url);
+    iframe.src = 'https://www.youtube.com/embed/3-BZxvKT6x4'; // url -> videoId
 
-//     const titleElement = this.element.querySelector(
-//       '.video__title'
-//     )! as HTMLParagraphElement;
-//     titleElement.textContent = title;
-//   }
-// }
+    const titleElement = this.element.querySelector(
+      '.video__title'
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+}
+
+// <iframe
+//   width="1354"
+//   height="762"
+//   src="https://www.youtube.com/embed/3-BZxvKT6x4"
+//   title="축신이라 불리는 사나이의 마지막 / 2022 FIFA 카타르월드컵 결승전 아르헨티나 vs 프랑스 [습츠_월드컵 클래식]"
+//   frameborder="0"
+//   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//   allowfullscreen
+// ></iframe>;
