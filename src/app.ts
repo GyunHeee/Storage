1b178be417ec679f7fc425bc08c771d74703ab14
GyunHee <ef3232@naver.com>
2023-04-01 13:41:00 +0900

    아이템 컨테이너 구현

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,33 @@
+import { Component } from './components/page/components.js';
 import { NoteComponent } from './components/page/item/Note.js';
 import { TodoComponent } from './components/page/item/Todo.js';
 import { VideoComponent } from './components/page/item/Video.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
-    this.page.attachTo(appRoot);
+    this.page.attachTo(appRoot, 'afterbegin');
 
     const video = new VideoComponent(
       'video Title',
       'https://www.youtube.com/watch?v=3-BZxvKT6x4'
     );
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
 
     const image = new ImageComponent(
       'image Title',
       'https://picsum.photos/200/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const note = new NoteComponent('note Title', 'note description');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo description');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
   }
 }
 
--- src/components/page/item/Video.ts
+++ src/components/page/item/Video.ts
@@ -14,13 +14,24 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     const iframe = this.element.querySelector(
       '.video__iframe'
     )! as HTMLIFrameElement;
-    console.log(url);
-    iframe.src = 'https://www.youtube.com/embed/3-BZxvKT6x4'; // url -> videoId
+    iframe.src = this.convertToEmbeddedURL(url); // url -> videoId
 
     const titleElement = this.element.querySelector(
       '.video__title'
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  // 정규표현식
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
 
